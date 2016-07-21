d5134243a2e1a26c594248adb8eb8c264ed97904
rahulv <rahulv@techaspect.com>
2016-07-21 18:00:59 +0530

    updated code and added skills.json

--- js/app.js
+++ js/app.js
@@ -1,26 +1,40 @@
 (function(){
-	var app = angular.module('profile',['ngRoute']);
-	
+	var app = angular.module('profile',['ngAnimate','ngRoute']);
+	//controller for routing
+	app.controller('HeaderController', ['$scope','$route', function($scope,$route){
+		$scope.showSlider = false;
+		$scope.$route = $route;
+	}])
+	//controller for skill set
+	app.controller('skillController', ['$http','$scope', function($http,$scope){
+		$http.get('js/skills.json').success(function(data){
+			$scope.skills= data.skills;
+		})
+	}])
 	// configure our routes
 	app.config(function($routeProvider) {
 		$routeProvider
 
 			// route for the home page
-			.when('/home', {
+			.when('/about', {
 				templateUrl : 'pages/about.html',
+				activetab : 'about'
 				
 			})
 			// route for the about page
-			.when('/about', {
+			.when('/portfolio', {
 				templateUrl : 'pages/home.html',
-			})
-			// default home
-			.otherwise ({
-				redirectTo : '/home'
+				activetab : 'portfolio'
 			})
 			// route for the contact page
 			.when('/contact', {
 				templateUrl : 'pages/contact.html',
+				activetab : 'contact'
+			})
+			// default home
+			.otherwise ({
+				redirectTo : '/about',
+				activetab : 'about'
 			})
 	});
 
