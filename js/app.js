365c7e4a186ad1bab9e8f438df814a48d84ef0bf
Rahul <rahulv@techaspect.com>
2016-08-31 16:31:54 +0530

    multiple changes

--- js/app.js
+++ js/app.js
@@ -1,22 +1,48 @@
 (function() {
-    var app = angular.module('profile', ['ngAnimate', 'ngRoute']);
+    var app = angular.module('profile', ['ngAnimate', 'ngRoute']).run(function($rootScope, $http) {
+        //loading json file for project info once only
+        $http.get('js/projects.json').success(function(data) {
+            $rootScope.projects = data.projects;
+        })
+        $rootScope.$on('$routeChangeStart', function (event, next) {
+            if(next.activetab != 'projects')
+                $rootScope.flag = false;
+            //console.log(next);
+            $(window).scrollTop(0);
+        })
+    });
+    //main controller 
+    app.controller('mainController',function($scope){
+        $scope.showslider = false;
+    })
     //controller for routing
     app.controller('HeaderController', ['$scope', '$route', function($scope, $route) {
-            $scope.showSlider = false;
             $scope.$route = $route;
         }])
         //controller for skill set
     app.controller('skillController', ['$http', '$scope', function($http, $scope) {
-            $http.get('js/skills.json').success(function(data) {
-                $scope.skills = data.skills;
-            })
-        }])
+        $http.get('js/skills.json').success(function(data) {
+            $scope.skills = data.skills;
+        })
+    }])
+    // adding class to ng-view
+    app.controller('classController', function($rootScope){
+        $rootScope.addScalingClass = function(){
+            $rootScope.flag = true;
+        }
+
+    })
+    //controller for loading particular project through route parameters
+    app.controller('projectDescriptionController', function($scope, $routeParams, $rootScope) {
+            var index = parseInt($routeParams.key);
+            $scope.record = $rootScope.projects[index - 1];
+        })
         //controller for profile summary
     app.controller('profileSummaryController', function($http, $scope, $timeout) {
             $scope.loader = true;
             $http.get('js/profile-summary.json').success(function(data) {
                     $scope.summary = data.summary;
-                    console.log(data.summary.length);
+                    //console.log(data.summary.length);
                     // assigning json data to a variable profiledata
                     $scope.profiledata = data;
                     // limiting the list initially to 5 only        
@@ -35,10 +61,11 @@
         })
         // controller for hiding and showing skill list
     app.controller('skillListController', ['$scope', function($scope) {
-            $scope.skillset = true;
+        $scope.skillset = true;
 
-        }])
-        // configure our routes
+    }])
+
+    // configure our routes
     app.config(function($routeProvider) {
         $routeProvider
 
@@ -57,6 +84,10 @@
             .when('/contact', {
                 templateUrl: 'pages/contact.html',
                 activetab: 'contact'
+            })
+            .when('/projects/:key', {
+                templateUrl: 'pages/projects.html',
+                activetab: 'projects'
             })
             // default home
             .otherwise({
